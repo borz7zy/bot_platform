50db344dbcac6ed43fab4eed840a158308de60e6
borz7zy <borz7zy@yandex.ru>
2024-09-20 14:28:05 +0200

    Added -WIN64 in logprint lib

--- libs/logprint/logprint.hxx
+++ libs/logprint/logprint.hxx
@@ -1,5 +1,4 @@
-
-#ifdef _WIN32 || _WIN64
+#if defined(_WIN32) || defined(_WIN64)
 #define LIB_EXPORT extern "C" __declspec(dllexport)
 #elif defined(__linux__)
 #define LIB_EXPORT extern "C" __attribute__((visibility("default")))
